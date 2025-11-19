eae8afb6a362fbd9cea44e61de19fb0afd87fe39
Cláudio Guerra <c.guerra@campus.fct.unl.pt>
2025-11-19 16:31:08 +0000

    remove all vue files

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <PageHeader></PageHeader>
-  <main>
-    <RouterView />
-  </main>
-</template>
-<script lang="ts" setup>
-import PageHeader from "@/components/PageHeader.vue";
-</script>
--- src/directives.ts
+++
@@ -1,6 +0,0 @@
-const directives = {
-  focus: { mounted: (el: HTMLInputElement) => el.focus() },
-  unfocus: { mounted: (el: HTMLInputElement) => el.blur() },
-};
-
-export default directives;
